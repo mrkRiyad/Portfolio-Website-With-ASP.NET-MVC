dfa36be58631bf5f42a871459d12ac414459ade2
mrkRiyad <dev.riyad@gmail.com>
2018-07-09 23:34:45 +0600

    initial file setup

---
+++ ScratchWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScratchWebsite.MvcApplication" Language="C#" %>
---
+++ ScratchWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScratchWebsite.Startup))]
+namespace ScratchWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
