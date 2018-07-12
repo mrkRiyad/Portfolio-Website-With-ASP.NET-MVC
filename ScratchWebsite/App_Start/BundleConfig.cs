b95219d18d4c2224db51fc445612ac58d47d7abb
mrkRiyad <dev.riyad@gmail.com>
2018-07-13 02:18:40 +0600

    setup admin ui

--- ScratchWebsite/App_Start/BundleConfig.cs
+++ ScratchWebsite/App_Start/BundleConfig.cs
@@ -23,9 +23,35 @@ namespace ScratchWebsite
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/Admin/adminScript").Include(
+                        "~/Areas/Admin/Assets/js/popper.min.js",
+                        "~/Areas/Admin/Assets/js/bootstrap.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/Admin/adminPlugin").Include(
+                        "~/Areas/Admin/Assets/js/plugins/jquery-ui.custom.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/bootstrap-datepicker.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/bootstrap-notify.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/jquery.dataTables.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/dataTables.bootstrap.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/moment.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/pace.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/select2.min.js",
+                        "~/Areas/Admin/Assets/js/plugins/sweetalert.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/Admin/adminPluginActivator").Include(
+                        "~/Areas/Admin/Assets/js/main.js"
+                        ));
+
+            bundles.Add(new StyleBundle("~/Admin/AdminCss").Include(
+                      "~/Areas/Admin/Assets/css/main.css"
+                      ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
